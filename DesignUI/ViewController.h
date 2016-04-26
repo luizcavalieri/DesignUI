c8b80775cf245456f2c9a41853aba82593849557
Luiz cavalieri <luiz.cavalieri@gmail.com>
2016-04-26 14:03:41 +1000

    Initial Commit

---
+++ DesignUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignUI
+//
+//  Created by Luiz cavalieri on 26/04/2016.
+//  Copyright © 2016 The Interactive Hippo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DesignUI/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DesignUI
+//
+//  Created by Luiz cavalieri on 26/04/2016.
+//  Copyright © 2016 The Interactive Hippo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
